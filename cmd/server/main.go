59bf7d7c6b0c71a9a6b66e66ca53beadb6538eef
Juan Manuel Abanto <94788498+juanmabam@users.noreply.github.com>
2022-03-03 11:32:00 -0500

    test with dapr

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,75 +2,144 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
+	"net/http"
 
-	_ "github.com/sofisoft-tech/ms-measureunit/docs"
-
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
-	"github.com/sofisoft-tech/ms-measureunit/internal/ports"
-	"github.com/sofisoft-tech/ms-measureunit/internal/service"
-	"github.com/sofisoft-tech/ms-measureunit/internal/validations"
-	"github.com/sofisoft-tech/ms-measureunit/seedwork/managers"
-	"github.com/sofisoft-tech/ms-measureunit/seedwork/middleware"
-	echoSwagger "github.com/swaggo/echo-swagger"
+	"github.com/dapr/go-sdk/service/common"
+	daprd "github.com/dapr/go-sdk/service/http"
 )
 
-// @title MeasureUnit API
-// @version v1
-// @description Specifying services for measure units.
-
-// @contact.name Sofisoft Technologies SAC
-// @contact.url https://sofisoft.pe
-// @contact.email juan.abanto@sofisoft.pe
-
-// @license.name MIT License
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	// create a Dapr service (e.g. ":8080", "0.0.0.0:8080", "10.1.1.1:8080" )
+	s := daprd.NewService(":8080")
+
+	// add some topic subscriptions
+	sub := &common.Subscription{
+		PubsubName: "messages",
+		Topic:      "topic1",
+		Route:      "/events",
+	}
+	if err := s.AddTopicEventHandler(sub, eventHandler); err != nil {
+		log.Fatalf("error adding topic subscription: %v", err)
 	}
 
-	router := echo.New()
-	ctx := context.Background()
-
-	application := service.NewApplication(ctx)
-
-	Handler(ports.NewHttpServer(application), router)
-	router.Logger.Fatal(router.Start(":3000"))
-}
-
-type ServerInterface interface {
-	AddMeasureType(c echo.Context) error
-	DeleteMeasureType(c echo.Context) error
-	GetMeasureType(c echo.Context) error
-	ListMeasureType(c echo.Context) error
-	UpdateMeasureType(c echo.Context) error
-}
-
-func Handler(si ServerInterface, router *echo.Echo) {
-	if router == nil {
-		router = echo.New()
+	// add a service to service invocation handler
+	if err := s.AddServiceInvocationHandler("/echo", echoHandler); err != nil {
+		log.Fatalf("error adding invocation handler: %v", err)
 	}
 
-	router.Validator = validations.NewValidationUtil()
+	// add an input binding invocation handler
+	if err := s.AddBindingInvocationHandler("/run", runHandler); err != nil {
+		log.Fatalf("error adding binding handler: %v", err)
+	}
 
-	loggerManager := managers.NewLoggerManager("https://services.sofisoft.pe/logging/", "ms-measureunit")
-
-	api := router.Group("/api/v1")
-
-	api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		LoggerErrorFunc: loggerManager.Error,
-	}))
-
-	//Swagger
-	router.GET("/*", echoSwagger.WrapHandler)
-
-	//measureType
-	api.DELETE("/measuretypes/:id", si.DeleteMeasureType)
-	api.GET("/measuretypes", si.ListMeasureType)
-	api.GET("/measuretypes/:id", si.GetMeasureType)
-	api.PATCH("/measuretypes", si.UpdateMeasureType)
-	api.POST("/measuretypes", si.AddMeasureType)
+	if err := s.Start(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("error listenning: %v", err)
+	}
 }
+
+func eventHandler(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
+	log.Printf("event - PubsubName:%s, Topic:%s, ID:%s, Data: %s", e.PubsubName, e.Topic, e.ID, e.Data)
+	return false, nil
+}
+
+func echoHandler(ctx context.Context, in *common.InvocationEvent) (out *common.Content, err error) {
+	if in == nil {
+		err = errors.New("invocation parameter required")
+		return
+	}
+
+	log.Printf(
+		"echo - ContentType:%s, Verb:%s, QueryString:%s, %s",
+		in.ContentType, in.Verb, in.QueryString, in.Data,
+	)
+	out = &common.Content{
+		Data:        in.Data,
+		ContentType: in.ContentType,
+		DataTypeURL: in.DataTypeURL,
+	}
+	return
+}
+
+func runHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
+	log.Printf("binding - Data:%s, Meta:%v", in.Data, in.Metadata)
+	return nil, nil
+}
+
+// package main
+
+// import (
+// 	"context"
+// 	"log"
+
+// 	_ "github.com/sofisoft-tech/ms-measureunit/docs"
+
+// 	"github.com/joho/godotenv"
+// 	"github.com/labstack/echo/v4"
+// 	"github.com/sofisoft-tech/ms-measureunit/internal/ports"
+// 	"github.com/sofisoft-tech/ms-measureunit/internal/service"
+// 	"github.com/sofisoft-tech/ms-measureunit/internal/validations"
+// 	"github.com/sofisoft-tech/ms-measureunit/seedwork/managers"
+// 	"github.com/sofisoft-tech/ms-measureunit/seedwork/middleware"
+// 	echoSwagger "github.com/swaggo/echo-swagger"
+// )
+
+// // @title MeasureUnit API
+// // @version v1
+// // @description Specifying services for measure units.
+
+// // @contact.name Sofisoft Technologies SAC
+// // @contact.url https://sofisoft.pe
+// // @contact.email juan.abanto@sofisoft.pe
+
+// // @license.name MIT License
+// // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// func main() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	router := echo.New()
+// 	ctx := context.Background()
+
+// 	application := service.NewApplication(ctx)
+
+// 	Handler(ports.NewHttpServer(application), router)
+// 	router.Logger.Fatal(router.Start(":3000"))
+// }
+
+// type ServerInterface interface {
+// 	AddMeasureType(c echo.Context) error
+// 	DeleteMeasureType(c echo.Context) error
+// 	GetMeasureType(c echo.Context) error
+// 	ListMeasureType(c echo.Context) error
+// 	UpdateMeasureType(c echo.Context) error
+// }
+
+// func Handler(si ServerInterface, router *echo.Echo) {
+// 	if router == nil {
+// 		router = echo.New()
+// 	}
+
+// 	router.Validator = validations.NewValidationUtil()
+
+// 	loggerManager := managers.NewLoggerManager("https://services.sofisoft.pe/logging/", "ms-measureunit")
+
+// 	api := router.Group("/api/v1")
+
+// 	api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+// 		LoggerErrorFunc: loggerManager.Error,
+// 	}))
+
+// 	//Swagger
+// 	router.GET("/*", echoSwagger.WrapHandler)
+
+// 	//measureType
+// 	api.DELETE("/measuretypes/:id", si.DeleteMeasureType)
+// 	api.GET("/measuretypes", si.ListMeasureType)
+// 	api.GET("/measuretypes/:id", si.GetMeasureType)
+// 	api.PATCH("/measuretypes", si.UpdateMeasureType)
+// 	api.POST("/measuretypes", si.AddMeasureType)
+// }
--- go.mod
+++ go.mod
@@ -2,16 +2,13 @@ module github.com/sofisoft-tech/ms-measureunit
 
 go 1.17
 
-require (
-	github.com/joho/godotenv v1.4.0
-	github.com/labstack/echo/v4 v4.6.3
-	go.mongodb.org/mongo-driver v1.8.2
-)
+require github.com/joho/godotenv v1.4.0
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/dapr/dapr v1.6.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
@@ -19,40 +16,44 @@ require (
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
 	github.com/swaggo/swag v1.7.6 // indirect
 	golang.org/x/tools v0.1.8 // indirect
+	google.golang.org/genproto v0.0.0-20210831024726-fe130286e0e2 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
 require (
+	github.com/dapr/go-sdk v1.3.1
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/echo-swagger v1.2.0
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
 )
